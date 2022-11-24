a4be9e4467d6f9a571db195c415254f95b8d839e
kiboma <omayioben@gmail.com>
2022-11-24 17:39:54 +0300

    Add role to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-    acts_as_token_authentication_handler_for User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :lockable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
