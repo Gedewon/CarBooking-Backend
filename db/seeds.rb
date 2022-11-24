35a0cb7a99e4dc750b2a9fd4fbde8c9bc3adfd5e
kiboma <omayioben@gmail.com>
2022-11-24 18:40:22 +0300

    Create random users and add username

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#User.create!(email: 'omayioben@gmail.com', password: '123456', password_confirmation: '123456', username: 'omayioben', admin: true)
+User.create!(email: 'omayioben12@gmail.com', password: '123456', password_confirmation: '123456', username: 'omayioben22', admin: true)
+User.create!(email: 'omayioben33@gmail.com', password: '123456', password_confirmation: '123456', username: 'omayioben33', admin: true)
+User.create!(email: 'omayioben44@gmail.com', password: '123456', password_confirmation: '123456', username: 'omayioben44', admin: true)
+User.create!(email: 'omayioben55@gmail.com', password: '123456', password_confirmation: '123456', username: 'omayioben55', admin: true)
+User.create!(email: 'omayioben66@gmail.com', password: '123456', password_confirmation: '123456', username: 'omayioben66', admin: true)
