c49c8949e03567ce866ef0e3c5df465245694824
kiboma <omayioben@gmail.com>
2022-11-24 18:15:19 +0300

    Create or own authenticate_user method for authenticating users with JWTs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,32 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+    protect_from_forgery with: :null_session
+    respond_to :json
+    before_action :authenticate_user
+
     before_action :underscore_params!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.for(:sign_up) << :username
     end
+  
+    def authenticate_user
+      if request.headers['Authorization'].present?
+        authenticate_or_request_with_http_token do |token|
+          begin
+            jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+  
+            @current_user_id = jwt_payload['id']
+          rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+            head :unauthorized
+          end
+        end
+      end
+    end
+
+    
 end
