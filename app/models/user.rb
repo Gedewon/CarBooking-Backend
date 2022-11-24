14cf11896b2a59cb3561dfe88fdf95111d301ed7
kiboma <omayioben@gmail.com>
2022-11-24 17:44:55 +0300

    Add a validation to the User model for usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
 
   def generate_jwt
     JWT.encode({ id: id,
