01e1cc2146a6bb30bc7625fbda65aa24d8d5f536
kiboma <omayioben@gmail.com>
2022-11-25 16:18:10 +0300

    Add car model and controllers

---
+++ db/migrate/20221125131721_create_cars.rb
@@ -0,0 +1,11 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.string :name
+      t.string :image
+      t.float :amount
+
+      t.timestamps
+    end
+  end
+end
