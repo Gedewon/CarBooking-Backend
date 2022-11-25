251cdb654f53990f68da26f890afab6bebb44358
gedewon <gedewon.hayle.9@gmail.com>
2022-11-25 22:15:30 +0300

    Create seession model to register multiple session for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_25_180442) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_190805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_25_180442) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "last_used_at"
+    t.boolean "status"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["last_used_at"], name: "index_sessions_on_last_used_at"
+    t.index ["status"], name: "index_sessions_on_status"
+    t.index ["token"], name: "index_sessions_on_token", unique: true
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
@@ -30,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_25_180442) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "sessions", "users"
 end
