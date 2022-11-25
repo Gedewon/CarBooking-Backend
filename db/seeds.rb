8c2a8f90844cd6f8235932b3b7bfe729042e3f04
kiboma <omayioben@gmail.com>
2022-11-25 16:40:34 +0300

    Create sample cars

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Car.create!(name: "Blue Bmw", image: "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg", amount: 22500)
+Car.create!(name: "Red Alfa Romeo C4 ", image: "https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg", amount: 18500)
+Car.create!(name: "Yellow Chevroelt ", image: "https://images.pexels.com/photos/575386/pexels-photo-575386.jpeg", amount: 37500)
+Car.create!(name: "Black Ferrari", image: "https://images.pexels.com/photos/9596562/pexels-photo-9596562.jpeg", amount: 19600)
+Car.create!(name: "Sports Car", image: "https://images.pexels.com/photos/4674337/pexels-photo-4674337.jpeg", amount: 44700)
