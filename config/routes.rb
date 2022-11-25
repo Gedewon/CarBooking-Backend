679aedf999c23ef1cc10cf19a4bd0823d4c43042
kiboma <omayioben@gmail.com>
2022-11-25 16:26:32 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :cars
+  #resources :cars
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :cars
+    end
+  end
+
 end
