84a7a951040737145f79749d052eeb4379317f95
gedewon <gedewon.hayle.9@gmail.com>
2022-11-26 03:10:26 +0300

    Create Reservations Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_125_190_805) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_26_000919) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cars', force: :cascade do |t|
-    t.string 'name'
-    t.string 'image'
-    t.float 'amount'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'sessions', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.datetime 'last_used_at'
-    t.boolean 'status'
-    t.string 'token'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['last_used_at'], name: 'index_sessions_on_last_used_at'
-    t.index ['status'], name: 'index_sessions_on_status'
-    t.index ['token'], name: 'index_sessions_on_token', unique: true
-    t.index ['user_id'], name: 'index_sessions_on_user_id'
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["city"], name: "index_reservations_on_city"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'password_digest'
-    t.string 'role', default: 'website_user'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "last_used_at"
+    t.boolean "status"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["last_used_at"], name: "index_sessions_on_last_used_at"
+    t.index ["status"], name: "index_sessions_on_status"
+    t.index ["token"], name: "index_sessions_on_token", unique: true
+    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
-  add_foreign_key 'sessions', 'users'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "unique"
+    t.string "password_digest"
+    t.string "role", default: "website_user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
+  add_foreign_key "sessions", "users"
 end
