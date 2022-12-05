1b98c53b2c21281e61fb8d14702fee33189d9204
gedewon <gedewon.hayle.9@gmail.com>
2022-12-05 05:25:50 +0300

    Create Admin user from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,5 @@ Car.create!(name: 'Red Alfa Romeo C4 ', image: 'https://images.pexels.com/photos
 Car.create!(name: 'Yellow Chevroelt ', image: 'https://images.pexels.com/photos/575386/pexels-photo-575386.jpeg', amount: 37_500,car_type:'lux',brand:'tesla',color:'red')
 Car.create!(name: 'Black Ferrari', image: 'https://images.pexels.com/photos/9596562/pexels-photo-9596562.jpeg', amount: 19_600,car_type:'lux',brand:'tesla',color:'red')
 Car.create!(name: 'Sports Car', image: 'https://images.pexels.com/photos/4674337/pexels-photo-4674337.jpeg', amount: 44_700,car_type:'lux',brand:'tesla',color:'red')
+
+User.create!(name:'Gedewon',image_url:'https://avatars.githubusercontent.com/u/56429354?v=4',role:'admin',email:'gedewon.hayle.9@gmail.com',password:'123123',password_confirmation:'123123',date_of_birth:' Tue, 05 Dec 1922')
